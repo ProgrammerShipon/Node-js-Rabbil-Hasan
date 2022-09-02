0b6ce2ac1cd17c2bccacec5a29c2da2eef74b4a8
Shipon Hossen Raju <msshipon234@gmail.com>
2022-09-02 19:44:13 +0600

    set cookies

---
+++ Node-with-Express.js/12-Response-Set-Cookies/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+app = express();
+
+/**
+ *  res.send() // response Body ke nidesh kore
+ * res.end()   // response ending point
+ */
+
+// Redirect
+app.get("/cooki", function(req, res) {
+   res.cookie('name' , 'Shipon Hossen Raju');
+   res.cookie('city' , 'Dhaka');
+   res.cookie('age' , '19 Years');
+   res.end('cookie seccess...');
+});
+
+
+
+// Server Start
+app.listen(5000, function () {
+   console.log('Server Run Successs.. Port: 5000 ');
+});
