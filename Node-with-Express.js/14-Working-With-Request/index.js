a5e14d66a6e7b3def7b67a3a32bbfba299d64b16
Shipon Hossen Raju <msshipon234@gmail.com>
2022-09-02 21:44:27 +0600

    Working Request Express js

---
+++ Node-with-Express.js/14-Working-With-Request/index.js
@@ -0,0 +1,28 @@
+/**
+ * Workin With Request
+ */
+
+/**
+ * Request Method 
+ *  Post()
+ *  Get()
+ *  Put()
+ *  Delete()
+ */
+
+/**
+ * Request get
+ *  Create simple get Request
+ *  Simple Get qequest with url parameter
+ *  catch request header simple get method
+ */
+
+/**
+ * Request post()
+ *  Request 
+ *  Request with url parameter
+ *  Request Header
+ *  Request json body
+ *  Request Multipart form data
+ *  Request file upload
+ */
