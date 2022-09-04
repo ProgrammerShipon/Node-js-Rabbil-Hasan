8395144ab7a776da2d55721ea35c49bcb8441a1a
Shipon Hossen Raju <msshipon234@gmail.com>
2022-09-04 16:27:59 +0600

    Multipart Form data istall => npm install --save multer

---
+++ Node-with-Express.js/22-Multipart-Form-Data/index.js
@@ -0,0 +1,35 @@
+  
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+/*
+{
+	"name" : "Shipon Hossen Raju",
+	"city": "pabna",
+	"age" : "19"
+} 
+app.post('/', function (req , res) {
+   let JSONData = req.body;
+   let JSONString = JSON.stringify(JSONData);
+   res.send(JSONString);
+})
+
+*/
+
+app.post('/', function(req, res) {
+   const JSONData = req.body;
+   const name = JSONData['name'];
+   const city = JSONData['city'];
+   const age = JSONData['age'];
+   res.send(`Name : ${name} , City : ${city} age : ${age}`)
+})
+
+// Server Start
+app.listen(1010, function () {
+   console.log('Server Run Successs.. Port: 1010 ');
+});
+
