34a36a3c9045da62ef52274c3b0edc0de2d01d09
Shipon Hossen Raju <msshipon234@gmail.com>
2022-09-07 18:19:35 +0600

    Custom Event Server to Client

---
+++ Socket.IO/11-Custom-Event-Server-to-Client/index.js
@@ -0,0 +1,44 @@
+// get http 
+const http = require('http');
+
+// Express Library and Configu
+const express = require('express');
+const app = express();
+
+// Create Express Server
+const expressServer = http.createServer(app);
+
+// Get Socket.io Server
+const { Server } = require('socket.io');
+const oi = new Server(expressServer);
+
+// Send Data to Client
+oi.on ('connection', function (socket) {
+   console.log('New User Connected');
+
+   // Custom Paramiter
+   setInterval(function () {
+      let d = new Date();
+      let t = d.getTime();
+
+      socket.emit('DateTime', t);
+   }, 100);
+
+   // User Disconnected
+   socket.on('disconnect', function () {
+      console.log('User Disconnected');
+   })
+})
+
+// Send File
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/index.html');
+})
+
+
+
+// Server Run 
+expressServer.listen(1010, function () {
+   console.log('Server Running @ port : 1010');
+})
+
