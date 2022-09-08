22d8243fa87e49013980ebe117a2039165eee80f
Shipon Hossen Raju <msshipon234@gmail.com>
2022-09-08 10:39:10 +0600

    Custom Event Server Data Transfer Client

---
+++ Socket.IO/13-Custom-Event-Server-Data-Transfer/index.js
@@ -0,0 +1,41 @@
+// get http 
+const http = require('http');
+
+// Express Library and Configu
+const express = require('express');
+const app = express();
+
+// Create Express Server
+const expressServer = http.createServer(app);
+
+// Get Socket.io Server
+const { Server } = require('socket.io');
+const oi = new Server(expressServer);
+
+// Send Data to Client
+oi.on ('connection', function (socket) {
+   console.log('New User Connected');
+
+   // Recive Data
+   socket.on('MyEvent', function (msg) {
+      console.log(msg);
+   })
+
+   // User Disconnected
+   socket.on('disconnect', function () {
+      console.log('User Disconnected');
+   })
+})
+
+// Send File
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/index.html');
+})
+
+
+
+// Server Run 
+expressServer.listen(1010, function () {
+   console.log('Server Running @ port : 1010');
+})
+
